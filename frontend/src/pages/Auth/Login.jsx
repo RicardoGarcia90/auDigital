6c4206ba9f007810159dcd557171ec3dac958c7b
Ricardo Garcia <ricardonunesgarcia@outlook.com>
2024-06-04 15:28:09 -0300

    feat: iniciando alteracoes de responsividade

--- frontend/src/pages/Auth/Login.jsx
+++ frontend/src/pages/Auth/Login.jsx
@@ -6,16 +6,18 @@ import LoginForm from './LoginForm';
 const Login = () => {
   return (
     <>
-      <div className="flex justify-center items-center py-24 gap-3">
-        <div className="bg-white grid grid-cols-2 items-center w-9/12 shadow-2xl">
+      <div className="flex justify-center items-center py-4 gap-3 ">
+        <div className="bg-white grid md:grid-cols-1 lg:grid-cols-2">
           {/* IMAGEM LOGIN */}
           <div>
-            <img src={loginImg} alt="Imagem de login" />
+            <img src={loginImg} alt="Imagem de login" className="mb-6" />
           </div>
 
           {/* FORM */}
           <div className="px-36">
-            <h3 className="text-center text-3xl font-bold mb-3">Login</h3>
+            <h3 className="text-center md:text-2xl  lg:text-3xl  font-bold mb-3">
+              Login
+            </h3>
             <p className="text-center">Preencha os campos para acessar</p>
             <LoginForm />
           </div>
