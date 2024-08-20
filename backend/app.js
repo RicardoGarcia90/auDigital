f9b1c694646c98c71e5e373da8e3c1404779d085
Ricardo Garcia <ricardonunesgarcia@outlook.com>
2024-08-20 19:20:40 -0300

    feat: criando get clients API

--- backend/app.js
+++ backend/app.js
@@ -1,12 +1,21 @@
+const fs = require('fs');
 const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.status(200).json({ message: 'Hello from the server!', app: 'auDigital' });
+const clients = JSON.parse(fs.readFileSync(`${__dirname}/data/clients.json`));
+
+app.get('/api/v1/clients', (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    results: clients.length,
+    data: {
+      clients,
+    },
+  });
 });
 
-// CONTINUAR EM AULA 51 - API AND RESTFULL API DESIGN
+// CONTINUAR EM 53-HANDING POST REQUESTS
 
 const port = 3000;
 app.listen(port, () => {
